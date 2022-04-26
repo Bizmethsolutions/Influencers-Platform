7fb09bbd8d67189558dd7cef6e5d28e48c5d6b63
Pranjal0490 <95899852+Pranjal0490@users.noreply.github.com>
2022-04-26 14:34:24 +0530

    Api integrated

--- resources/views/agency/edit.blade.php
+++ resources/views/agency/edit.blade.php
@@ -1,13 +1,5 @@
 @extends('layout.agencymaster')
 @section('content')
-<!--Plugin CSS file with desired skin-->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
-
-<!--jQuery-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!--Plugin JavaScript file-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js"></script>
 <style>
     .form-control{
         min-height:47px;
@@ -30,53 +22,18 @@
         min-height: 47px;
     }
     </style>
-  <style>
-    #loader{
-      background-color: #fff;
-    }
-    .loader {
-      position: fixed;
-      z-index: 999;
-      border: 16px solid #f3f3f3;
-      border-radius: 50%;
-      border-top: 16px solid #3498db;
-      width: 120px;
-      height: 120px;
-      margin: auto;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      -webkit-animation: spin 2s linear infinite; /* Safari */
-      animation: spin 2s linear infinite;
-    }
-
-    /* Safari */
-    @-webkit-keyframes spin {
-      0% { -webkit-transform: rotate(0deg); }
-      100% { -webkit-transform: rotate(360deg); }
-    }
-
-    @keyframes spin {
-      0% { transform: rotate(0deg); }
-      100% { transform: rotate(360deg); }
-    }
-  </style>
-  <div id="loader" style="width:100vw;height:100vh;background-color: #fff;display: none;">
-    <div class="loader"></div>
-  </div>
   <div class="container-fluid">
     <div class="page-title">
       <div class="row">
         <div class="col-6">
           <h3>
-             Search</h3>
+             Edit Brand</h3>
         </div>
         <div class="col-6">
           <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="index-2.html"><i data-feather="home"></i></a></li>
             <li class="breadcrumb-item">Dashboard</li>
-            <li class="breadcrumb-item active">Search</li>
+            <li class="breadcrumb-item active">Edit Brand</li>
           </ol>
         </div>
       </div>
@@ -92,14 +49,8 @@
           {{ session()->get('error') }}
       </div>
   @endif
-  <style type="text/css">
-    .form-control{
-      min-height: 40px !important;
-    }
-  </style>
   <?php
-
-$lang = [
+  $lang = [
     ["code" => "ab", "name" => "Abkhaz", "nativeName" => "аҧсуа"],
     ["code" => "aa", "name" => "Afar", "nativeName" => "Afaraf"],
     ["code" => "af", "name" => "Afrikaans", "nativeName" => "Afrikaans"],
@@ -708,268 +659,94 @@ $countries = [
     ["name" => "Zambia", "code" => "ZM"],
     ["name" => "Zimbabwe", "code" => "ZW"]
 ];
-
   ?>
   <!-- Container-fluid starts-->
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-3">
-          <label>Country</label>
-          <select class="form-control" name="InfluencerCountry" id="city">
-            <option value="">Select Country</option>
-            <?php 
-              foreach($countries as $c){
-                ?>
-                  <option value="<?php echo $c['name']; ?>"><?php echo $c['name']; ?></option>    
-                <?php    
-              }
-            ?>
-            
-
-          </select>
-        </div>
-        <div class="col-md-3">
-          <label>Language</label>
-          <select class="form-control" name="InfluencerLanguage" id="language">
-            <option value="">Select Language</option>
-            <?php 
-              foreach($lang as $c){
-                ?>
-                  <option value="<?php echo $c['name']; ?>"><?php echo $c['name']; ?></option>    
-                <?php    
-              }
-            ?>
-            
-          </select>
-        </div>
-        <div class="col-md-3">
-          <label>Gender</label>
-          <select class="form-control" name="InfluencerGender" id="gender">
-              <option value="">Gender</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-          </select>
-        </div>
-        <div class="col-md-3">
-          <label>Influencer Interest</label><br>
-          <select class="form-control" name="InfluencerInterest" id="foi">
-              <option></option>
-              @foreach($foi as $value)
-                <option value="{{ $value->name }}">{{ $value->name }}</option>
-              @endforeach
-          </select>
-        </div>
-      </div>
-      <div class="row" style="margin-top:10px;">
-        <div class="col-md-3">
-          <label>Brands</label>
-          <input type="text" class="form-control" name="InfluencerBrands">
-        </div>
-        <div class="col-md-3">
-          <label>Followers</label><br>
-          <!-- <input type="text" id="js-range-slider" name="my_range" value=""
-              data-type="double"
-              data-min="0"
-              data-max="1000"
-              data-from="200"
-              data-to="500"
-              data-grid="true"
-          />   -->
-          <div class="range-slider">
-              <input type="text" class="js-range-slider" value="" />
-          </div>
-          <div class="extra-controls">
-              <input type="hidden" name="follower_min" class="js-input-from" value="0" />
-              <input type="hidden" name="follower_max" class="js-input-to" value="0" />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <label>Age</label><br>
-          <div class="range-slider">
-              <input type="text" class="js-range-slider1" value="" />
-          </div>
-          <div class="extra-controls">
-              <input type="hidden" name="age_min" class="age-from" value="0" />
-              <input type="hidden" name="age_max" class="age-to" value="0" />
-          </div>
-        </div>
-
-        <div class="col-md-3">
-          <input type="button" name="" onclick="return search();" value="Search" class="btn btn-primary" style="margin-top: 15px;">
-        </div>
-      </div>
-    </div>
-    <div class="container-fluid" style="margin-top: 20px;">
-      
-      <div class="row" id="searchResult">
-        @foreach($list as $val) 
-        <div class="col-md-6 col-lg-6 col-xl-4 box-col-6">
-          <div class="card custom-card p-0">
-            <div class="card-header"><img class="img-fluid" src="../assets/images/user-card/1.jpg" alt=""></div>
-            <div class="card-profile"><img class="rounded-circle" src="../assets/images/avtar/3.jpg" alt=""></div>
-            <ul class="card-social">
-              <li><a href="#"><img src="../assets/images/facebook.png" style="width:24px;"></a></li>
-              
-              <li><a href="#"><img src="../assets/images/instagram.png" style="width:24px;"></a></li>
-              
-            </ul>
-            <div class="text-center profile-details">
-              <h5>{{ $val }}</h5>
-              <h6>Influencer</h6>
-            </div>
-            <div class="card-footer row">
-              <div class="col-4 col-sm-4">
-                <h6>Follower</h6>
-                <h5 class="counter">9564</h5>
+  <div class="container-fluid">
+    <div class="row size-column">
+      <div class="col-xl-12 box-col-12 xl-100">
+        <form class="theme-form" method="POST" action="{{ url('agency/updateBrands')}}">
+            @if(Session::get('error'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{Session::get('error')}}
               </div>
-              <div class="col-4 col-sm-4">
-                <h6>Following</h6>
-                <h5><span class="counter">49</span>K</h5>
+            @endif
+            
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label class="col-form-label">Brand Name</label>
+                  <input class="form-control" value="{{ $brands->name }}" type="text" required="" name="name">
+                </div>
               </div>
-              <div class="col-4 col-sm-4">
-                <h6>Total Post</h6>
-                <h5><span class="counter">96</span>M</h5>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label class="col-form-label">Email Address</label>
+                  <input class="form-control" value="{{ $brands->email }}" type="email" required="" name="email" placeholder="Test@gmail.com">
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        @endforeach
-        
+            <input type="hidden" name="id" value="{{ $brands->id }}">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label class="col-form-label">Brand Type</label> 
+                  <select class="form-control" name="brand_type" required="" id="foi">
+                    <option></option>
+                    @foreach($foi as $value)
+                      <option value="{{ $value->name }}" <?php if($brands->brand_type == $value->name ){ echo "selected";} ?>>{{ $value->name }}</option>
+                    @endforeach
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label class="col-form-label">Brand Language</label> 
+                  <select class="form-control" required="" name="language" id="language">
+                    <option value="">Select Language</option>
+                    <?php 
+                      foreach($lang as $c){
+                        ?>
+                          <option value="<?php echo $c['name']; ?>" <?php if($brands->language == $c['name'] ){ echo "selected";} ?>><?php echo $c['name']; ?></option>    
+                        <?php    
+                      }
+                    ?>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label class="col-form-label">Brand Country</label> 
+                  <select class="form-control" name="city" required="" id="city">
+                    <option></option>
+                    <?php
+                    foreach($countries as $c){
+                        ?>
+                          <option value="<?php echo $c['name']; ?>" <?php if($brands->city == $c['name'] ){ echo "selected";} ?>><?php echo $c['name']; ?></option>    
+                        <?php    
+                    }
+                    ?>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label class="col-form-label">Brand Agency</label> 
+                  <input class="form-control" value="{{ $brands->agency }}" type="text" required="" name="agency">
+                </div>
+              </div>
+            </div>
+
+            {!! csrf_field() !!}
+            <div class="row" style="margin-top: 10px;">
+              <div class="form-group mb-0">
+                <button class="btn btn-primary btn-block" type="submit">Submit</button>
+              </div>
+            </div>
+          </form>
       </div>
     </div>
-  <!-- Container-fluid Ends-->
+  </div>
 @endsection
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script type="text/javascript">
-// $.getScript('https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.0/js/ion.rangeSlider.min.js',function(){
-//     $("#js-range-slider").ionRangeSlider({
-//         type: "double",
-//         min: 0,
-//         max: 1000,
-//         from: 200,
-//         to: 500,
-//         onStart: updateInputs,
-//         onChange: updateInputs,
-//         grid: true
-//      });
-//   }); //script
-$.getScript('https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.0/js/ion.rangeSlider.min.js',function(){
-  var $range = $(".js-range-slider"),
-    $inputFrom = $(".js-input-from"),
-    $inputTo = $(".js-input-to"),
-    instance,
-    min = 0,
-    max = 1000,
-    from = 0,
-    to = 0;
-
-  var $range1 = $(".js-range-slider1"),
-    $agefrom = $(".age-from"),
-    $ageto = $(".age-to"),
-    instance1,
-    agemin = 18,
-    agemax = 50,
-    agefrom = 0,
-    ageto = 0;
-
-$range.ionRangeSlider({
-  skin: "round",
-    type: "double",
-    min: min,
-    max: max,
-    from: 200,
-    to: 800,
-    onStart: updateInputs,
-    onChange: updateInputs
-});
-instance = $range.data("ionRangeSlider");
-$range1.ionRangeSlider({
-  skin: "round",
-    type: "double",
-    min: agemin,
-    max: agemax,
-    from: 20,
-    to: 40,
-    onStart: ageupdateInputs,
-    onChange: ageupdateInputs
-});
-instance1 = $range1.data("ionRangeSlider");
-
-function updateInputs (data) {
-  from = data.from;
-    to = data.to;
-    
-    $inputFrom.prop("value", from);
-    $inputTo.prop("value", to); 
-}
-function ageupdateInputs (data) {
-    agefrom = data.from;
-    ageto = data.to;
-    
-    $agefrom.prop("value", agefrom);
-    $ageto.prop("value", ageto); 
-}
-
-$inputFrom.on("input", function () {
-    var val = $(this).prop("value");
-    
-    // validate
-    if (val < min) {
-        val = min;
-    } else if (val > to) {
-        val = to;
-    }
-    
-    instance.update({
-        from: val
-    });
-});
-
-$agefrom.on("input", function () {
-    var val = $(this).prop("value");
-    
-    // validate
-    if (val < min) {
-        val = min;
-    } else if (val > to) {
-        val = to;
-    }
-    
-    instance1.update({
-        from: val
-    });
-});
-
-$inputTo.on("input", function () {
-    var val = $(this).prop("value");
-    
-    // validate
-    if (val < from) {
-        val = from;
-    } else if (val > max) {
-        val = max;
-    }
-    
-    instance.update({
-        to: val
-    });
-});
-
-$ageto.on("input", function () {
-    var val = $(this).prop("value");
-    
-    // validate
-    if (val < from) {
-        val = from;
-    } else if (val > max) {
-        val = max;
-    }
-    
-    instance1.update({
-        to: val
-    });
-});
-
-
-});
-</script>
